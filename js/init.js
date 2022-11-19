c56b4c24cc2cf7a0bb5b25f4e87290ebb13a4262
mrhlalumnos42 <118584005+mrhlalumnos42@users.noreply.github.com>
2022-11-18 21:48:01 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+   apiKey: "AIzaSyC0YhCbnRQXKXYs16ExgRbEEfWmntvs0q4",
+  authDomain: "mrhlalumnos42-11d12.firebaseapp.com",
+  projectId: "mrhlalumnos42-11d12",
+  storageBucket: "mrhlalumnos42-11d12.appspot.com",
+  messagingSenderId: "646276784887",
+  appId: "1:646276784887:web:d745ea7c2780ebfbdd29f2",
+  measurementId: "G-K39YFQQT4X"
 });
